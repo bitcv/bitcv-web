e00e3094eb80c845101d4c667b3ab520a362f056
junderking <junderking@163.com>
2018-03-11 20:17:14 +0800

    rebuild

--- server/public/static/js/23.232398a3216fd6507524.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([23],{Ws3R:function(e,t){},Z8Fx:function(e,t,n){"use strict";Object.defineProperty(t,"__esModule",{value:!0});var r={render:function(){var e=this.$createElement,t=this._self._c||e;return t("div",{staticClass:"container"},[t("router-view")],1)},staticRenderFns:[]};var a=n("VU/8")({data:function(){return{}}},r,!1,function(e){n("Ws3R")},"data-v-63dbc864",null);t.default=a.exports}});
-//# sourceMappingURL=23.232398a3216fd6507524.js.map
